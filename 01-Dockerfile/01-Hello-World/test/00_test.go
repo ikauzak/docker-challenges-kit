954553369194770f21079e5567ecbb60edabcf74
Bruno Masuda <brunokazuaki@gmail.com>
2022-09-22 11:31:59 -0300

    fix(test): removido comments

--- 01-Dockerfile/01-Hello-World/test/00_test.go
+++ 01-Dockerfile/01-Hello-World/test/00_test.go
@@ -13,22 +13,13 @@ const tag = "v1"
 const fullImageName = image + ":" + tag
 
 // Função para validar se a imagem existe dentro do host
-func TestIfImageExist(t *testing.T) {
+func TestIfImageExists(t *testing.T) {
 	output := docker.DoesImageExist(t, fullImageName, logger.TestingT)
 	assert.True(t, output)
 }
 
-// Função para validar output do arquivo dentro da imagem
+// Função para validar a saída do arquivo que está dentro da imagem
 func TestDockerHelloWorldExample(t *testing.T) {
-	/*
-		tag := "gruntwork/docker-hello-world-example"
-		buildOptions := &docker.BuildOptions{
-			Tags: []string{tag},
-		}
-
-		docker.Build(t, "../", buildOptions)
-	*/
-
 	opts := &docker.RunOptions{Command: []string{"cat", "/test.txt"}, Remove: true}
 	output := docker.Run(t, fullImageName, opts)
 	assert.Equal(t, "Hello, World!", output)
