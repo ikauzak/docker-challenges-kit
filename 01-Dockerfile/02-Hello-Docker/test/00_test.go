e5f4b4fd12969ad270758e912657d9dd8606cabe
Bruno Masuda <brunokazuaki@gmail.com>
2022-09-24 08:32:56 -0300

    feat(tests): alterado outputs do logger para Discard

--- 01-Dockerfile/02-Hello-Docker/test/00_test.go
+++ 01-Dockerfile/02-Hello-Docker/test/00_test.go
@@ -14,12 +14,17 @@ const fullImageName = image + ":" + tag
 
 // Função para validar se a imagem existe dentro do host
 func TestIfImageExists(t *testing.T) {
-	output := docker.DoesImageExist(t, fullImageName, logger.TestingT)
-	assert.True(t, output)
+	t.Logf("Validando se a imagem %s existe...", fullImageName)
+	output := docker.DoesImageExist(t, fullImageName, logger.Discard)
+	if assert.True(t, output) != true {
+		t.Fatalf("Imagem %s não encontrada!", fullImageName)
+	}
+	t.Logf("Imagem %s encontrada com sucesso!", fullImageName)
 }
 
 // Função para validar a saída do arquivo que está dentro da imagem
 func TestDockerHelloWorldExample(t *testing.T) {
+	t.Logf("Validando o arquivo /test.txt existe na imagem %s...", fullImageName)
 	opts := &docker.RunOptions{Command: []string{"cat", "/test.txt"}, Remove: true}
 	output := docker.Run(t, fullImageName, opts)
 	assert.Equal(t, "Hello, Docker!", output)
